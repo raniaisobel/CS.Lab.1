18dcc27a149e82d78712d3e239967ef06a0a4211
Rania <rania.isobel@gmail.com>
2024-08-26 08:35:04 +0800

    Finished my first lab!

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age: ");
+        String age = scanner.nextLine();
+        int ageNumb = Integer.parseInt(age);
+        System.out.println(age+"-year olds should date somebody who is at least "+(7+(ageNumb/2))+" years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age: ");
+        String age = scanner.nextLine();
+        int ageNumb = Integer.parseInt(age);
+        System.out.println(age+"-year olds should read at least "+(100-ageNumb)+" pages before giving up on a book.");
     }
 }
